0b0afb4b50ee4b1687baad699b2f3691bc8d196e
Abigail Wiggins <rebtaylor14@gmail.com>
2019-11-11 13:12:07 -0600

    Class3 studio refactored

--- src/Studios/CountingCharacters.java
+++ src/Studios/CountingCharacters.java
@@ -14,7 +14,6 @@ public class CountingCharacters {
         input.close();
 
         String sampleString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there";
-
         sampleString = sampleString.toLowerCase();
 
         char[] charactersInString = inputString.toCharArray();
@@ -23,19 +22,21 @@ public class CountingCharacters {
         TreeMap<Character, Integer> charCounts = new TreeMap<>();
         //loop through the char array
         for (char i : charactersInString) {
-            int count = 0;
-            for (char j : charactersInString) {
-                if (i == j) {
-                    count++;
+            if (Character.isLetter(i) && !charCounts.containsKey(i)) {
+                int count = 0;
+                for (char j : charactersInString) {
+                    if (i == j) {
+                        count++;
+                    }
                 }
-            }
-            //initialize or add char variable
-            if (Character.isLetter(i)) {
+                //initialize or add char variable
                 charCounts.put(i, count);
+                System.out.println(i + ": "+ count);
             }
-            //use TreeMap or sort the HashMap
+
         }
-        System.out.println(charCounts);
+
+
 
     }
 
